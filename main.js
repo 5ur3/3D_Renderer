3319e0fbfc8ffacd69aab473b814b467b22595c5
5ur3 <an30.work@gmail.com>
2019-01-31 17:28:29 +0400

    Незначительное изменение

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ let amp = 10;
 
 let angle = 0;
 
-//function update() {
+function update() {
 	camera.position = new Vector(Math.sin(angle * speed) * amp, Math.cos(angle * speed * 1.7) * 2 + 0.5, Math.cos(angle * speed) * amp);
 	cube1.rotation.add(new Vector(0.1, 3, 0));
 	camera.look(Vector.substract(cube1.position, camera.position));
 
 	angle += Math.PI / 180;
 	render(camera);
-//}
+}
